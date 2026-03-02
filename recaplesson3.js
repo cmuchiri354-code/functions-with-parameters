387b5797d7796f03e66b4af06a0bf4ef411f8ad9
cmuchiri354-code <cmuchiri354@gmail.com>
2026-03-02 13:47:48 +0300

    java functions

---
+++ recaplesson3.js
@@ -0,0 +1,89 @@
+// Do the following 
+// function to add 
+function addition() {
+    let num1 = 6
+    let num2 = 4
+    let answer = num1 + num2
+    console.log("the sum of the two is" + answer)
+}
+
+// call the function 
+addition()
+
+
+
+// function to subtract 
+function subtract (){
+    let number1 = 10
+    let number2 = 4
+    let answer = number1 - number2
+    console.log("the difference of the two is" + answer)
+}
+
+// call the function 
+subtract()
+
+
+
+
+
+// function to multiply 
+function multiplication (){
+    let number1 = 10
+    let number2 = 4
+    let answer = number1 * number2
+    console.log("the product of the two is" + answer)
+}
+
+// call the function 
+multiplication()
+
+
+
+
+
+// function to divide 2 nos 
+function  divide(){
+    let num1 = 24
+    let num2 = 8
+    let answer = num1/num2
+    console.log("the quotent of the two is" + answer)
+}
+
+
+// call the function 
+divide()
+
+
+
+
+
+// function to find simple interest
+// simple interest = p*r*t
+function simpleinterest(){
+    let p =20000
+    let r =0.1
+    let t = 5
+    let answer =p*r*t
+    console.log("the simple interest is"+ answer)
+}
+
+
+// call the function 
+simpleinterest()
+
+
+// function to find bmi 
+// bmi = weight / height * height 
+function bmi(){
+    let weight = 16000
+    let height = 4
+    let answer = weight/(height*height)
+    console.log("the bmi is " + answer)
+}
+
+
+// call the function 
+bmi()
+
+
